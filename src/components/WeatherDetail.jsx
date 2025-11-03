ee4fc174612f82dfd8bf22a3c6cb46c0a0805d94
chavimiller <chaviweisman@gmail.com>
2025-11-03 12:33:24 +0200

    Create components for weather data, add to global css file

---
+++ src/components/WeatherDetail.jsx
@@ -0,0 +1,11 @@
+import styles from "./WeatherDetail.module.css";
+
+const WeatherDetail = () => {
+  return (
+    <>
+      <div className={styles.container}>Weather Detail</div>
+    </>
+  );
+};
+
+export default WeatherDetail;
---
+++ src/components/WeatherOverview.jsx
@@ -0,0 +1,10 @@
+import styles from "./WeatherOverview.module.css";
+const WeatherOverview = () => {
+  return (
+    <>
+      <div className={styles.container}>Main Overview</div>
+    </>
+  );
+};
+
+export default WeatherOverview;
