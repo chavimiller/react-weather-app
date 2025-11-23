55530817f7ac6a6d99e4f4f10d24a269809f6568
chavimiller <chaviweisman@gmail.com>
2025-11-23 15:33:59 +0200

    Add logic to convert epoch timestamps and format hourly weather data

--- src/components/WeatherDetail.jsx
+++ src/components/WeatherDetail.jsx
@@ -1,16 +1,44 @@
+import { toZonedTime } from "date-fns-tz";
 import { useWeatherContext } from "../context/WeatherContext";
 import HourlyData from "./HourlyData";
 import styles from "./WeatherDetail.module.css";
 import { WeatherStats } from "./WeatherStats";
+import { format } from "date-fns-tz";
 
 const WeatherDetail = () => {
-  const { weather, loading, error } = useWeatherContext();
+  const { weather, loading } = useWeatherContext();
+
+  if (loading || !weather) return <div>Loading...</div>;
+
+  const currentHour = new Date().getHours();
 
   const condition = (hour) => {
     return weather.days[0].hours[hour].conditions === "Partially cloudy"
       ? "Cloudy"
       : weather.days[0].hours[hour].conditions;
   };
+
+  const degrees = (hour) => {
+    return Math.round(weather.days[0].hours[hour].temp);
+  };
+
+  const epoch = (hour) => {
+    return weather.days[0].hours[hour].datetimeEpoch;
+  };
+
+  const timezone = weather.timezone;
+
+  const time = (timezone, epoch) => {
+    const formattedTime = format(
+      toZonedTime(epoch * 1000, timezone),
+      "h:mm a",
+      {
+        timezone: timezone,
+      }
+    );
+    return formattedTime;
+  };
+
   return (
     <>
       <div className={styles.container}>
@@ -18,24 +46,24 @@ const WeatherDetail = () => {
           <div className={styles.detailTitle}>Hourly Forecast</div>
           <div className={styles.hourlyFrame}>
             <HourlyData
-              desc={condition(19)}
-              degrees={Math.round(weather.days[0].hours[19].temp)}
-              time={"6:00am"}
+              desc={condition(currentHour)}
+              degrees={degrees(currentHour)}
+              time={time(timezone, epoch(currentHour))}
             />
             <HourlyData
-              desc={condition(20)}
-              degrees={Math.round(weather.days[0].hours[20].temp)}
-              time={"7:00am"}
+              desc={condition(currentHour + 1)}
+              degrees={degrees(currentHour + 1)}
+              time={time(timezone, epoch(currentHour + 1))}
             />
             <HourlyData
-              desc={condition(21)}
-              degrees={Math.round(weather.days[0].hours[21].temp)}
-              time={"8:00am"}
+              desc={condition(currentHour + 2)}
+              degrees={degrees(currentHour + 2)}
+              time={time(timezone, epoch(currentHour + 2))}
             />
             <HourlyData
-              desc={condition(22)}
-              degrees={Math.round(weather.days[0].hours[22].temp)}
-              time={"9:00am"}
+              desc={condition(currentHour + 3)}
+              degrees={degrees(currentHour + 3)}
+              time={time(timezone, epoch(currentHour + 3))}
             />
           </div>
           <div className={styles.statsFrame}>
